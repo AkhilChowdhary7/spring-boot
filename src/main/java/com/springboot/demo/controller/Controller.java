21edfa0f01e16953ddb8c32c66b14bdc5a55a3ab
AkhilChowdhary7 <akhil2422001@gmail.com>
2024-07-09 13:58:46 -0400

    tests

--- src/main/java/com/springboot/demo/controller/Controller.java
+++ src/main/java/com/springboot/demo/controller/Controller.java
@@ -1,12 +1,15 @@
 package com.springboot.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "*")
 @RequestMapping("/spring-boot")
 public class Controller {
     @GetMapping()
